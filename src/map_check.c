2cd6233bb01eec947c49e7015e6497b0c76289d2
Mohammad Hossein Isi Mohassel <m.mohassel1997@gmail.com>
2023-04-14 12:30:51 +0200

    added maps, fix mlx42

--- src/map_check.c
+++ src/map_check.c
@@ -6,13 +6,13 @@
 /*   By: misi-moh <misi-moh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 13:54:28 by misi-moh          #+#    #+#             */
-/*   Updated: 2023/04/08 18:01:40 by misi-moh         ###   ########.fr       */
+/*   Updated: 2023/04/14 12:06:18 by misi-moh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-/*int has_no_valid_path(t_map *map)
+int has_no_valid_path(t_map *map)
 {
     check_map_path(map);
 }
@@ -23,16 +23,16 @@ int empty(char *map_string)
         return (TRUE);
     return (FALSE);
 }
-*/
+
 
 int map_has_errors(char *argv)
 {
-    /*char    *map_string;
+    char    *map_string;
     t_map   *map;
-*/
+
     if(not_valid_map_name(argv))
         return (TRUE);
-    /*map_string = read_map(argv);
+    map_string = read_map(argv);
     if (empty(map_string))
         return(empty_map(map_string));
     if (is_missing_components(map_string))
@@ -43,6 +43,6 @@ int map_has_errors(char *argv)
         return (structure_error(map));
     if (has_no_valid_path(map))
         return(no_valid_path(map));
-    destroy_structure(map);*/
+    destroy_structure(map);
     return (FALSE);
 }
